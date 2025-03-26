e34b9ad014539878caa746e415a850618c396fb2
I-M-Marinov <lcfrrr@gmail.com>
2025-03-26 19:54:34 +0200

    dashboard implemented

--- Exam Preparations/JS Applications Regular Exam - 10 August 2024/Tattoo-Masters/src/views/dashboard.js
+++ Exam Preparations/JS Applications Regular Exam - 10 August 2024/Tattoo-Masters/src/views/dashboard.js
@@ -1,7 +1,30 @@
 import { html, render  } from '../lib/litHtml.js';
+import { getAllTattoos } from '../services/tattoesService.js';
 
-const template = () => html``;
+const template = (tattoos) => html`<h2>Collection</h2>
+  <section id="tattoos">
+    ${tattoos?.length
+      ? tattoos.map(
+          (t) =>
+            html`<div class="tattoo">
+              <img src="${t.imageUrl}" />
+              <div class="tattoo-info">
+                <h3 class="type">${t.type}</h3>
+                <span>Uploaded by </span>
+                <p class="user-type">${t.userType}</p>
+                <a class="details-btn" href="/dashboard/${t._id}">Learn More</a>
+              </div>
+            </div>`
+        )
+      : html
+      `
+        <h2 id="no-tattoo">
+          Collection is empty, be the first to contribute
+        </h2>`}
+  </section>`;
 
-export function dashboardView(){
-    render(template());
-}
+export async function dashboardView() {
+  const tattoos = await getAllTattoos();
+
+  render(template(tattoos));
+};
